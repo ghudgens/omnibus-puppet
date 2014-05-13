18aa90f2d42fc42e4380f0db2020ae589949f3a7
Grant Hudgens <grant.hudgens@gmail.com>
2014-05-13 10:17:25 -0400

    Ensuring header is on all files.

--- config/software/cacerts.rb
+++ config/software/cacerts.rb
@@ -1,3 +1,19 @@
+#
+# Copyright:: Copyright (c) 2014 Grant Hudgens.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 name "cacerts"
 default_version "2014.04.22"  # date of the file is in a comment at the start, or in the changelog
 
