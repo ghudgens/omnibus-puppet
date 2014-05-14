e8ecc25f334989424d0ce08c87de0a8b3fa69009
Grant Hudgens <grant.hudgens@gmail.com>
2014-05-13 20:24:04 -0400

    Removing hiera install from puppet-gem config.

--- config/software/puppet-gem.rb
+++ config/software/puppet-gem.rb
@@ -21,6 +21,5 @@ dependency "ruby"
 dependency "rubygems"
 
 build do
-  gem "install hiera -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v '1.3.2'"
   gem "install puppet -n #{install_dir}/bin --no-rdoc --no-ri -v #{version}"
 end
