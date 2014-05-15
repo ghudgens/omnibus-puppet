e18a73b624cd964f5e12382014909817e404fe8d
Grant Hudgens <grant.hudgens@gmail.com>
2014-05-15 15:07:23 -0400

    Adding apr-util to puppetmaster project.

--- config/projects/puppetmaster.rb
+++ config/projects/puppetmaster.rb
@@ -30,6 +30,7 @@ override :'hiera-gem', version: "1.3.2"
 override :'facter-gem', version: "2.0.1"
 override :'puppet-gem', version: "#{build_version}"
 override :'apr', version: "1.5.1"
+override :'apr-util', version: "1.5.3"
 
 #Creates required build directories
 dependency 'preparation'
@@ -39,6 +40,7 @@ dependency 'facter-gem'
 dependency 'puppet-gem'
 dependency 'hiera-gem'
 dependency 'apr'
+dependency 'apr-util'
 
 #Puppet Master and its dependencies' configuration/misc files
 dependency 'puppetmaster-files'
