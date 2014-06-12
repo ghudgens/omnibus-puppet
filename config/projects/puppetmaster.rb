919004bcbcb2fd984e60a5199c86add97f7a99b1
Grant Hudgens <grant.hudgens@spacex.com>
2014-06-11 23:09:16 -0400

    Adding augeas and sinatra gems as foreman proxy is pretty dependent on them...

--- config/projects/puppetmaster.rb
+++ config/projects/puppetmaster.rb
@@ -25,6 +25,8 @@ build_iteration 2
 
 # Specify dependency versions to embed
 override :'ruby', version: "2.0.0-p451"
+override :'augeas-gem', version: "0.5.0"
+override :'sinatra-gem', version: "1.4.5"
 override :'hiera-gem', version: "1.3.2"
 override :'hiera-eyaml-gem', version: "2.0.2"
 override :'facter-gem', version: "2.0.1"
@@ -40,6 +42,8 @@ dependency 'preparation'
 dependency 'librarian-puppet-gem'
 dependency 'facter-gem'
 dependency 'puppet-gem'
+dependency 'augeas-gem'
+dependency 'sinatra-gem'
 dependency 'hiera-gem'
 dependency 'hiera-eyaml-gem'
 dependency 'passenger-gem'
